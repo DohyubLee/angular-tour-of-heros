c202bc0f01416fde41dde0ad9c9f6f1f949aecf4
DohyubLee <hani7304@gmail.com>
2018-04-06 05:40:54 +0900

    4. Displaying a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';  //hero.ts에서 만든 클래스를 여기서 사용하려면 import해야함
+import { HEROES } from '../mock-heroes';
 
 @Component({  //메타데이터
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css']   //이 CSS파일은 이 컴포넌트에서만 적용됨
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;  //변수에 타입 지정
 
   constructor() { }
 
   ngOnInit() {  //초기화 로직을 넣을 라이프 사이클 hook
+  }
+
+  onSelect(hero: Hero): void { //Hero타입의 매개변수를 받고 반환형은 void인 함수
+    this.selectedHero = hero;
   }
 
 }
